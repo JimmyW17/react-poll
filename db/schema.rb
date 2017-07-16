def24d447417b4114bbfa86eaf2506172e94b4d2
Jimmy Wu <wujimmy94@gmail.com>
2017-07-16 13:40:36 -0400

    added poll model, set up one to many relation between poll and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170716160102) do
+ActiveRecord::Schema.define(version: 20170716172727) do
+
+  create_table "polls", force: :cascade do |t|
+    t.string "title"
+    t.integer "choices"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
