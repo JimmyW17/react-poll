e3f048a29d482e6861e93d542bdbbb37204dcb16
Jimmy Wu <wujimmy94@gmail.com>
2017-07-16 14:02:23 -0400

    changed comments to become nested resource of poll

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
   resources :polls
+  resources :polls do
+    resources :comments
+  end
   devise_for :users
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
