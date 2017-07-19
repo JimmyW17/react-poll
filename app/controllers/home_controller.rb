fae00f0a334ff6d975244d32aff502c019ece72e
Jimmy Wu <wujimmy94@gmail.com>
2017-07-19 16:12:22 -0400

    react works, minor cleanups from old gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @test_props = {message: 'Testing react, this is a prop'}
+    @user = {name: 'Jimmy'}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     resources :comments
   end
   devise_for :users
-  get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
